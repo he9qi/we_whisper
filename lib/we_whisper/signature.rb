217cd70194edc4918f1c5529c7a4d4e7a2c997ab
Qi He <qhe@heyook.com>
2016-03-31 14:16:14 -0700

    update signature

--- lib/we_whisper/signature.rb
+++ lib/we_whisper/signature.rb
@@ -3,9 +3,8 @@ require 'digest/sha2'
 module WeWhisper
   module Signature
     def self.sign(token, timestamp, nonce, encrypted)
-      array = [token, timestamp, nonce]
-      array << encrypted unless encrypted.nil?
-      Digest::SHA1.hexdigest array.compact.collect(&:to_s).sort.join
+      Digest::SHA1.hexdigest \
+        [token, timestamp, nonce, encrypted].compact.collect(&:to_s).sort.join
     end
   end
 end
